8a2f30e8b7a38f43d88577a111ba5e3b530304b9
Alexey Vakhov <vakhov@gmail.com>
2014-11-30 16:13:22 +0300

    Cosmetic fixes in version spec

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -1,7 +1,7 @@
 require 'snowman-io'
 
-RSpec.describe 'SnowManIO Version' do
-  it "should contain 3 digits" do
+RSpec.describe SnowmanIO::VERSION do
+  it "contains 3 digits" do
     expect(SnowmanIO::VERSION).to match(/^\d+\.\d+\.\d+$/)
   end
 end
