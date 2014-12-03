3053f5820ad4c56df46c10caf205bf85c0782417
Alexey Vakhov <vakhov@gmail.com>
2014-12-03 23:50:58 +0300

    Simple capybora story

--- spec/snowman-io/api_spec.rb
+++ spec/snowman-io/api_spec.rb
@@ -1,4 +1,17 @@
 require 'spec_helper'
 
-RSpec.describe SnowmanIO::API do
+RSpec.describe SnowmanIO::API, type: :feature do
+  before do
+    @old_app = Capybara.app
+    Capybara.app = SnowmanIO::API
+  end
+
+  after do
+    Capybara.app = @old_app
+  end
+
+  it "unpacking" do
+    visit "/"
+    expect(current_path).to eq("/")
+  end
 end
