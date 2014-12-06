7060b6b1b7a1d8262f6b56d77fee95f0a8b428ee
Alexey Vakhov <vakhov@gmail.com>
2014-12-06 22:35:51 +0300

    Fix specs

--- spec/snowman-io/check_spec.rb
+++ spec/snowman-io/check_spec.rb
@@ -22,33 +22,42 @@ RSpec.describe SnowmanIO::Check do
   }
 
   context "Hosted Graphite" do
-    it "returns last value" do
-      expect(ENV).to receive(:[]).with("HG_KEY").and_return("/hg/key")
-      url = "https://www.hostedgraphite.com/hg/key/graphite/render?format=json&target=#{hg_target}&from=-10mins"
-      expect(check).to receive(:open).with(url) { StringIO.new(JSON.dump(hg_response)) }
+    context "with HG_KEY" do
+      before do
+        @old_hg_key = ENV["HG_KEY"]
+        ENV["HG_KEY"] = "/hg/key"
+      end
 
-      expect(check.send(:get_hg_value, hg_target)).to eq(9)
+      after do
+        ENV["HG_KEY"] = @old_hg_key
+      end
+
+      it "returns last value" do
+        url = "https://www.hostedgraphite.com/hg/key/graphite/render?format=json&target=#{hg_target}&from=-10mins"
+        expect(check).to receive(:open).with(url) { StringIO.new(JSON.dump(hg_response)) }
+
+        expect(check.send(:get_hg_value, hg_target)).to eq(9)
+      end
+
+      it "returns nil if empty dataset" do
+        url = "https://www.hostedgraphite.com/hg/key/graphite/render?format=json&target=#{hg_target}&from=-10mins"
+        expect(check).to receive(:open).with(url) { StringIO.new(JSON.dump(hg_empty_response)) }
+
+        expect(check.send(:get_hg_value, hg_target)).to be_nil
+      end
+
+      it "gets data for last 20 minutes" do
+        url = "https://www.hostedgraphite.com/hg/key/graphite/render?format=json&target=#{hg_target}&from=-20mins"
+        expect(check).to receive(:open).with(url) { StringIO.new(JSON.dump(hg_response)) }
+
+        expect(check.send(:get_hg_value, hg_target, from: '-20mins')).to eq(9)
+      end
     end
 
-    it "returns nil if empty dataset" do
-      expect(ENV).to receive(:[]).with("HG_KEY").and_return("/hg/key")
-      url = "https://www.hostedgraphite.com/hg/key/graphite/render?format=json&target=#{hg_target}&from=-10mins"
-      expect(check).to receive(:open).with(url) { StringIO.new(JSON.dump(hg_empty_response)) }
-
-      expect(check.send(:get_hg_value, hg_target)).to be_nil
-    end
-
-    it "returns nil unless HG_KEY env variable is setted" do
-      expect(ENV).to receive(:[]).with("HG_KEY").and_return(nil)
-      expect(check.send(:get_hg_value, hg_target)).to be_nil
-    end
-
-    it "gets data for last 20 minutes" do
-      expect(ENV).to receive(:[]).with("HG_KEY").and_return("/hg/key")
-      url = "https://www.hostedgraphite.com/hg/key/graphite/render?format=json&target=#{hg_target}&from=-20mins"
-      expect(check).to receive(:open).with(url) { StringIO.new(JSON.dump(hg_response)) }
-
-      expect(check.send(:get_hg_value, hg_target, from: '-20mins')).to eq(9)
+    context "without HG_KEY" do
+      it "returns nil" do
+        expect(check.send(:get_hg_value, hg_target)).to be_nil
+      end
     end
   end
 end
