e78ecb5c83a41553d5222ad4f26802818a6ca227
Artyom Keydunov <artyom.keydunov@gmail.com>
2014-12-09 00:34:42 -0800

    Use Ember.$ instead of $
    It makes jslint happier

--- ui/app/controllers/application.js
+++ ui/app/controllers/application.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   version: function() {
-     return $('meta[name=snowman-io-version]').attr("content");
+     return Ember.$('meta[name=snowman-io-version]').attr("content");
   }.property()
 });
