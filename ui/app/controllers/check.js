7a34e4142cd3b858055131019f80d82abef9c21c
Artyom Keydunov <artyom.keydunov@gmail.com>
2014-12-23 23:14:55 -0800

    [UI] Remove config import in controllers/check

--- ui/app/controllers/check.js
+++ ui/app/controllers/check.js
@@ -1,12 +1,10 @@
 import Ember from 'ember';
-import config from '../config/environment';
 
 export default Ember.Controller.extend({
   actions: {
     resolve: function() {
       this.set("resolveInProgress", true);
       var me = this;
-
       this.get("model").resolve().then(function() {
         me.get('model').reload().then(function() {
           me.set("resolveInProgress", false);
