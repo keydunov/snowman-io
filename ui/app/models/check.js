f584a4f67e33d3cca3de5571abd3be0e14992b28
Artyom Keydunov <artyom.keydunov@gmail.com>
2014-12-23 23:05:48 -0800

    Fix resolving check in ui
    Fixes #44

--- ui/app/models/check.js
+++ ui/app/models/check.js
@@ -6,10 +6,23 @@ export default DS.Model.extend({
   fail_count: DS.attr('number'),
   raw_history: DS.attr('string'),
 
+  resolve: function() {
+    return Ember.$.ajax({
+      url: this._resolveUrl(),
+      type: "POST",
+      data: JSON.stringify({})
+    });
+  },
+
+  _resolveUrl: function() {
+    var adapter = this.container.lookup('adapter:application');
+    return adapter.buildURL('checks', this.get('id')) + '/resolve'
+  },
+
   history: function() {
     return JSON.parse(this.get("raw_history"));
   }.property("raw_history"),
-  
+
   isFailed: function() {
     return this.get("status") === 'failed';
   }.property("status")
