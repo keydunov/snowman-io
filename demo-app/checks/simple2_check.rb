802f47df22d694dafe1116ed8b1589a3355b31cc
Alexey Vakhov <vakhov@gmail.com>
2015-01-02 22:37:51 +0300

    Remove checks from demo-app

--- demo-app/checks/simple2_check.rb
+++
@@ -1,7 +0,0 @@
-class Simple2Check < SnowmanIO::Check
-  interval 5.seconds
-
-  def ok?
-    true
-  end
-end
--- demo-app/checks/sys/sys_check.rb
+++
@@ -1,9 +0,0 @@
-module Sys
-  class SysCheck < SnowmanIO::Check
-    interval 5.seconds
-
-    def ok?
-      [false, true].sample
-    end
-  end
-end
