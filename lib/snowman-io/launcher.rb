dabb269a3bfdb73b274439c9fe451aff181b2724
Alexey Vakhov <vakhov@gmail.com>
2015-01-15 23:51:50 +0300

    Remove collectors on server

--- lib/snowman-io/launcher.rb
+++ lib/snowman-io/launcher.rb
@@ -8,7 +8,6 @@ module SnowmanIO
 
     def start
       @web_server_supervisor = WebServer.supervise_as(:web_server, API, @options.slice(:port, :host, :verbose))
-      @collect = Loop::Collect.supervise_as(:collect_loop)
       @ping = Loop::Ping.supervise_as(:ping_loop)
       @aggregate = Loop::Aggregate.supervise_as(:aggregate_loop)
       @report = Loop::Report.supervise_as(:report_loop)
@@ -18,7 +17,6 @@ module SnowmanIO
       @report.terminate
       @aggregate.terminate
       @ping.terminate
-      @collect.terminate
       @web_server_supervisor.terminate # TODO: shutdown blocking?
     end
   end
