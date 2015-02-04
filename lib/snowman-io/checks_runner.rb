aed403e2c96809877a7afc6757e36048f0db823f
Artyom Keydunov <artyom.keydunov@gmail.com>
2015-02-04 01:02:10 -0800

    Extract Check from ChecksRunner

--- lib/snowman-io/checks_runner.rb
+++ lib/snowman-io/checks_runner.rb
@@ -24,40 +24,6 @@ module SnowmanIO
     end
   end
 
-  class Check
-    def initialize(metric)
-      @metric = metric
-      # double check to be sure it is sorted by keys (timestamps)
-      # Do we need it?
-      @metric['daily'] = @metric['daily'].sort.to_h
-    end
-
-    # Returns alert or nil
-    def run
-      check if checkable?
-    end
-
-    private
-
-    def check
-      raise NotImplementedError
-    end
-
-    # Filter if check actually can be done against current metric
-    # We cannot check anythig if we do not have at least two days to compare
-    # TODO: not sure what criterias are for metric to be checkable
-    def checkable?
-      @metric['daily'].size > 1 &&
-      @metric['daily'].values.last['count'] > 0
-    end
-
-    def human(val)
-      Utils.human_value(val)
-    end
-  end
-
-  Alert = Struct.new(:metric_name, :check, :message)
-
   # Check if current value has increased
   # significantly compared to first value
   class IncreasedCheck < Check
@@ -65,14 +31,6 @@ module SnowmanIO
       if today_avg > first_avg*2
         Alert.new(@metric['name'], self.class.name, message)
       end
-    end
-
-    def today_avg
-      @metric['daily'].values.last['avg']
-    end
-
-    def first_avg
-      @metric['daily'].values.first['avg']
     end
 
     def first_date
@@ -91,18 +49,8 @@ module SnowmanIO
       end
     end
 
-    def today_avg
-      @metric['daily'].values.last['avg']
-    end
-
     def message
       "Today's #{@metric['name']} average (#{human(today_avg)}) is bigger than 1 sec."
     end
-
-    def human(val)
-      Utils.human_value(val)
-    end
   end
-
-  class CheckResult; end
 end
