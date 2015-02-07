f42fc71bd16c9d47fcd979eda4f496bc8bb9b712
Alexey Vakhov <vakhov@gmail.com>
2015-02-07 17:13:56 +0300

    Total clean - 2

--- ui/app/models/app.js
+++ ui/app/models/app.js
@@ -1,14 +1,15 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
   token: DS.attr('string'),
 
-  isNameEmpty: Ember.computed.empty('name'),
-
   // Rails app
   requestsJSON: DS.attr('string'),
 
+  // Computed
+  isNameEmpty: Ember.computed.empty('name'),
   requests: function () {
     return JSON.parse(this.get("requestsJSON"));
   }.property("requestsJSON")
--- ui/app/router.js
+++ ui/app/router.js
@@ -11,15 +11,6 @@ Router.map(function() {
 
   this.route('apps/new', {path: '/apps/new'});
   this.route('apps/edit', {path: '/apps/:id/edit'});
-
-  // this.route('apps/index', {path: '/apps'});
-  // this.route('apps/show', {path: '/apps/:id'});
-  //
-  // this.route('metrics/index', {path: '/metrics'});
-  // this.route('metrics/show', {path: '/metrics/:id'});
-  //
-  // this.route('reports/index', {path: '/reports'});
-  // this.route('reports/show', {path: '/reports/:id'});
 });
 
 export default Router;
