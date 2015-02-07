b49bb8c814680d662443a0bc2f8993bd05298de6
Alexey Vakhov <vakhov@gmail.com>
2015-02-08 00:33:23 +0300

    Fix specs

--- ui/app/controllers/dashboard.js
+++ ui/app/controllers/dashboard.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export default Ember.ObjectController.extend({
   actions: {
     destroy: function(app) {
-      var me = this;
       if (confirm("Are you sure?")) {
         app.deleteRecord();
         app.save();
