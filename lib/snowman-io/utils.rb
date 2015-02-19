48d8cdf071024bd7b1fa7685024b83553ffd1b11
Alexey Vakhov <vakhov@gmail.com>
2015-02-19 10:52:17 +0300

    Use `up` value instead of 90-percentile

--- lib/snowman-io/utils.rb
+++ lib/snowman-io/utils.rb
@@ -8,9 +8,27 @@ module SnowmanIO
       arr.inject(:+).to_f/arr.length
     end
 
-    # Returns `t` percentile for `arr`
-    def self.pct(arr, t)
-      arr.sort[(t*(arr.length - 1)).round]
+    # `up` value is close to 90 percentile by meaning, but it slightly
+    # depends of every element (weighted avarage). This metric is usefull for small arrays.
+    def self.up(arr)
+      sorted = arr.sort
+      if sorted.length <= 1
+        sorted[0]
+      else
+        sum = 0
+        amount = 0
+        sorted.each_with_index { |e, i|
+          w = i.to_f/sorted.length
+          if w <= 0.9
+            a = w + 0.1
+          else
+            a = 1.9 - w
+          end
+          sum += e*a
+          amount += a
+        }
+        sum/amount
+      end
     end
 
     def self.human_value(value)
--- spec/snowman-io/utils_spec.rb
+++ spec/snowman-io/utils_spec.rb
@@ -1,10 +1,16 @@
 require 'spec_helper'
 
 RSpec.describe SnowmanIO::Utils, type: :feature do
-  it "stats" do
+  it "avg" do
     a = [1, 2, 4, 4.5, 5]
     expect(SnowmanIO::Utils.avg(a)).to eq(3.3)
-    expect(SnowmanIO::Utils.pct(a, 0.1)).to eq(1)
-    expect(SnowmanIO::Utils.pct(a, 0.9)).to eq(5)
+  end
+
+  it "up" do
+    expect(SnowmanIO::Utils.up([1])).to eq(1)
+    expect(SnowmanIO::Utils.up([1, 5]).round(2)).to eq(4.43)
+    expect(SnowmanIO::Utils.up([1, 5, 1]).round(2)).to eq(3.36)
+    expect(SnowmanIO::Utils.up([1, 5, 5]).round(2)).to eq(4.69)
+    expect(SnowmanIO::Utils.up([1, 1, 1, 1, 1, 5]).round(2)).to eq(2.2)
   end
 end
