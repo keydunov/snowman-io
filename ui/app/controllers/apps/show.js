b3028b35996c7dc4081619eeba486e5d46aceb5a
Alexey Vakhov <vakhov@gmail.com>
2015-02-22 23:55:49 +0300

    Fix specs

--- ui/app/controllers/apps/show.js
+++ ui/app/controllers/apps/show.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
   hasRequests: function() {
-    return this.get("app.requests.today") && this.get("app.requests.yesterday")
+    return this.get("app.requests.today") && this.get("app.requests.yesterday");
   }.property()
 });
