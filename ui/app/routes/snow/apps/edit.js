46a2325b69c94166a62f336db2f5d032d5565c53
Alexey Vakhov <vakhov@gmail.com>
2015-04-05 14:23:57 +0300

    Cosmetic

--- ui/app/routes/snow/apps/edit.js
+++ ui/app/routes/snow/apps/edit.js
@@ -5,6 +5,11 @@ export default Ember.Route.extend({
     return this.store.find('app', params.id);
   },
 
+  setupController: function(controller, model) {
+    this._super(controller, model);
+    controller.set("fixedName", model.get("name"));
+  },
+
   deactivate: function() {
     if (this.controller.get("model.isDirty")) {
       this.controller.get("model").rollback();
