0fa53913b9f0dc056d7f093b9e2b418f977d159e
Alexey Vakhov <vakhov@gmail.com>
2015-04-05 21:06:39 +0300

    Cool menu

--- ui/app/routes/apps/show.js
+++ ui/app/routes/apps/show.js
@@ -1,6 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  activate: function() {
+    this.controllerFor("apps").set("showMenu", true);
+  },
+
+  deactivate: function() {
+    this.controllerFor("apps").set("showMenu", false);
+  },
+
   model: function(params) {
     return this.store.find('app', params.id);
   }
