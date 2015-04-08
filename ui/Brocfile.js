6e240923ed06736862c6b8ff6d92f2ceac8e7006
Alexey Vakhov <vakhov@gmail.com>
2015-04-08 11:25:38 +0300

    Return vendor bootstrap

--- ui/Brocfile.js
+++ ui/Brocfile.js
@@ -1,10 +1,17 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var app = new EmberApp();
 
-var app = new EmberApp({
-  'ember-cli-bootstrap-sass': {
-    'components': false,
-    'importBootstrapJS': true
-  }
+// boostrap
+app.import("vendor/bootstrap-3.3.1/css/bootstrap.css");
+app.import("vendor/bootstrap-3.3.1/js/bootstrap.js");
+var fonts = [
+  "vendor/bootstrap-3.3.1/fonts/glyphicons-halflings-regular.eot",
+  "vendor/bootstrap-3.3.1/fonts/glyphicons-halflings-regular.svg",
+  "vendor/bootstrap-3.3.1/fonts/glyphicons-halflings-regular.ttf",
+  "vendor/bootstrap-3.3.1/fonts/glyphicons-halflings-regular.woff"
+];
+fonts.forEach(function(font) {
+  app.import(font, {destDir: "fonts"});
 });
 
 module.exports = app.toTree();
