69b4f37ddfe10b8d1add40511d9a760af2ca9dd6
Alexey Vakhov <vakhov@gmail.com>
2015-04-08 10:41:24 +0300

    Use bootstrap from package

--- ui/Brocfile.js
+++ ui/Brocfile.js
@@ -1,20 +1,10 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
-var app = new EmberApp();
 
-// Bootstrap
-app.import("vendor/bootstrap-3.3.1/css/bootstrap.css");
-app.import("vendor/bootstrap-3.3.1/js/bootstrap.js");
-var fonts = [
-  "vendor/bootstrap-3.3.1/fonts/glyphicons-halflings-regular.eot",
-  "vendor/bootstrap-3.3.1/fonts/glyphicons-halflings-regular.svg",
-  "vendor/bootstrap-3.3.1/fonts/glyphicons-halflings-regular.ttf",
-  "vendor/bootstrap-3.3.1/fonts/glyphicons-halflings-regular.woff"
-];
-fonts.forEach(function(font) {
-  app.import(font, {destDir: "fonts"});
+var app = new EmberApp({
+  'ember-cli-bootstrap-sass': {
+    'components': false,
+    'importBootstrapJS': true
+  }
 });
-
-// D3.js
-app.import("vendor/d3/d3.js");
 
 module.exports = app.toTree();
