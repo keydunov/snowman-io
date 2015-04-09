427a7a2d6b621ec8f0ee199463e60c2f4ab8db66
Alexey Vakhov <vakhov@gmail.com>
2015-04-09 12:33:41 +0300

    Cosmetic

--- ui/app/controllers/apps/show/add_hg_metric.js
+++ ui/app/controllers/apps/show/add_hg_metric.js
@@ -1,15 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  // needs: ["snow"],
-  // baseUrl: Ember.computed.alias("controllers.snow.model.base_url"),
-  // hgStatus: Ember.computed.alias("controllers.snow.model.hg_status"),
-  // isHgEnabled: Ember.computed.equal("hgStatus", "enabled"),
+  resetForm: function() {
+    this.set("hgName", "");
+    this.set("hgMetricName", "");
+    this.set("hgKind", this.hgKinds[0]);
+  },
 
-  // hasRequests: function() {
-  //   return this.get("model.requests.today") && this.get("model.requests.yesterday");
-  // }.property("model.requests.today", "model.requests.yesterday"),
-  //
   hgKinds: ["time", "counter", "amount", "deploy"],
 
   hgFormValid: function() {
