7d85bd62b83fb51f51b06dd3dcddda38927071c2
Alexey Vakhov <vakhov@gmail.com>
2015-04-11 14:11:48 +0300

    Counter metric

--- ui/app/components/graphite-graph.js
+++ ui/app/components/graphite-graph.js
@@ -6,7 +6,6 @@ export default Ember.Component.extend({
   tagName: "img",
   attributeBindings: ["src"],
   kind: "default",
-  options: {},
 
   defaultGraphiteOptions: {
     lineMode: "connected",
@@ -14,13 +13,58 @@ export default Ember.Component.extend({
     fgcolor: "black",
     hideLegend: true,
     lineWidth: 1.5,
-    width: 1100,
-    height: 250
+    width: 200,
+    height: 200
   },
 
   customGraphiteOptions: function() {
-    return Ember.$.extend({target: this.get("hgMetric.metricName")}, this.get("defaultGraphiteOptions"));
-  }.property("hgMetric.metricName"),
+    var metric = this.get("hgMetric.metricName");
+
+    if (this.get("hgMetric.kind") == "counter" && this.get("duration") == "24h") {
+      return Ember.$.extend({}, this.get("defaultGraphiteOptions"), {
+        width: 800,
+        targets: [
+          "secondYAxis(color(timeShift(integral("+metric+"),'168h'),'magenta'))",
+          "secondYAxis(color(timeShift(integral("+metric+"),'144h'),'gray'   ))",
+          "secondYAxis(color(timeShift(integral("+metric+"),'120h'),'yellow' ))",
+          "secondYAxis(color(timeShift(integral("+metric+"),'96h' ),'orange' ))",
+          "secondYAxis(color(timeShift(integral("+metric+"),'72h' ),'purple' ))",
+          "secondYAxis(color(timeShift(integral("+metric+"),'48h' ),'brown'  ))",
+          "secondYAxis(color(timeShift(integral("+metric+"),'24h' ),'aqua'   ))",
+
+          "color(lineWidth(transformNull("+metric+",0),1.5),'0000ff33')",
+          "secondYAxis(color(lineWidth(integral("+metric+"),4),'green'))",
+        ]
+      });
+
+    } else if (this.get("hgMetric.kind") == "counter" && this.get("duration") == "1h") {
+      return Ember.$.extend({}, this.get("defaultGraphiteOptions"), {
+        from: '-1h',
+        width: 300,
+        targets: [
+          "secondYAxis(color(lineWidth(timeShift(integral("+metric+"),'11h'),2),'d0d0d0'))",
+          "secondYAxis(color(lineWidth(timeShift(integral("+metric+"),'10h'),2),'d0d0d0'))",
+          "secondYAxis(color(lineWidth(timeShift(integral("+metric+"),'9h' ),2),'d0d0d0'))",
+          "secondYAxis(color(lineWidth(timeShift(integral("+metric+"),'8h' ),2),'d0d0d0'))",
+          "secondYAxis(color(lineWidth(timeShift(integral("+metric+"),'7h' ),2),'d0d0d0'))",
+          "secondYAxis(color(lineWidth(timeShift(integral("+metric+"),'6h' ),2),'d0d0d0'))",
+          "secondYAxis(color(lineWidth(timeShift(integral("+metric+"),'5h' ),2),'d0d0d0'))",
+          "secondYAxis(color(lineWidth(timeShift(integral("+metric+"),'4h' ),2),'d0d0d0'))",
+          "secondYAxis(color(lineWidth(timeShift(integral("+metric+"),'3h' ),2),'d0d0d0'))",
+          "secondYAxis(color(lineWidth(timeShift(integral("+metric+"),'2h' ),2),'d0d0d0'))",
+          "secondYAxis(color(lineWidth(timeShift(integral("+metric+"),'1h' ),2),'d0d0d0'))",
+
+          "color(lineWidth(transformNull("+metric+",0),1.5),'0000ff33')",
+          "secondYAxis(color(lineWidth(integral("+metric+"),4),'green'))",
+        ]
+      });
+
+    } else {
+      return Ember.$.extend({}, this.get("defaultGraphiteOptions"), {
+        target: metric
+      });
+    }
+  }.property("hgMetric.kind", "hgMetric.metricName", "duration"),
 
   src: function() {
     var src = "https://www.hostedgraphite.com" + this.get("hgKey") + "/graphite/render?";
