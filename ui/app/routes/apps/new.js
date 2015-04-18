1e6b67956786b4667d97a991ab6f45e5664dff0b
Alexey Vakhov <vakhov@gmail.com>
2015-04-18 21:20:51 +0300

    Fix specs

--- ui/app/routes/apps/new.js
+++ ui/app/routes/apps/new.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   setupController: function(controller, model) {
+    this._super(controller, model);
     controller.set("appName", "");
   }
 });
