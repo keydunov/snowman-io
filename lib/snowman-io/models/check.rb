71a5815f7ec467f7a03faffa3507303fd6bde734
Artyom Keydunov <artyom.keydunov@gmail.com>
2015-04-19 19:42:12 +0300

    Fix creating new check

--- lib/snowman-io/models/check.rb
+++ lib/snowman-io/models/check.rb
@@ -5,5 +5,12 @@ module SnowmanIO
 
     field :cmp, type: String
     field :value, type: Float
+
+    def as_json(options = {})
+      super(options).tap do |o|
+        o["id"] = o.delete("_id").to_s
+        o["hg_metric_id"] = o["hg_metric_id"].to_s
+      end
+    end
   end
 end
