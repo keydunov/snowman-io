de3c8eda8c69b0181f3ef07b67b40005e8e5b8b3
Alexey Vakhov <vakhov@gmail.com>
2015-04-23 13:02:57 +0300

    Extract form

--- ui/app/controllers/snow/inline/hg_form.js
+++ ui/app/controllers/snow/inline/hg_form.js
@@ -1,68 +1,62 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  needs: ["snow"],
+  hgStatus: Ember.computed.alias("controllers.snow.model.hg_status"),
+  hgKey: Ember.computed.alias("controllers.snow.model.hg_key"),
+
   reset: function() {
-    console.log("reset");
+    this.set("saving", false);
+    this.set("show", false);
+    this.set("isHgDisabled", this.get("hgStatus") === "disabled");
+    this.set("hgFormKey", this.get("hgKey"));
   },
 
-  // needs: ["snow"],
-  // baseUrl: Ember.computed.alias("controllers.snow.model.base_url"),
-  // reportRecipients: Ember.computed.alias("controllers.snow.model.report_recipients"),
-  // hgStatus: Ember.computed.alias("controllers.snow.model.hg_status"),
-  // hgKey: Ember.computed.alias("controllers.snow.model.hg_key"),
-  //
-  // resetHgForm: function() {
-  //   this.set("hgSaving", false);
-  //   this.set("hgFormShow", false);
-  //   this.set("isHgDisabled", this.get("hgStatus") === "disabled");
-  //   this.set("hgFormKey", this.get("hgKey"));
-  // },
-  //
-  // isHgFormValid: function() {
-  //   return this.get("isHgDisabled") || Ember.isPresent(this.get("hgFormKey"));
-  // }.property("isHgDisabled", "hgFormKey"),
-  //
-  // hgStatusText: function() {
-  //   if (this.get("hgSaving")) {
-  //     return "saving...";
-  //   } else {
-  //     return this.get("hgStatus");
-  //   }
-  // }.property("hgStatus", "hgSaving"),
-  //
-  // actions: {
-  //   hgChange: function() {
-  //     this.set("hgFormShow", !this.get("hgFormShow"));
-  //   },
-  //
-  //   hgSelectEnabled: function() {
-  //     this.set("isHgDisabled", false);
-  //   },
-  //
-  //   hgSelectDisabled: function() {
-  //     this.set("isHgDisabled", true);
-  //   },
-  //
-  //   hgCancel: function() {
-  //     this.resetHgForm();
-  //   },
-  //
-  //   hgSave: function() {
-  //     var that = this;
-  //     var hgStatus = that.get('isHgDisabled') ? "disabled" : "enabled";
-  //     var hgKey = hgStatus === "enabled" ? that.get("hgFormKey") : "";
-  //     that.set("hgFormShow", false);
-  //     that.set("hgSaving", true);
-  //     Ember.$.post(that._baseUrl() + "/hg", {hg_status: hgStatus, hg_key: hgKey}, function(data) {
-  //       that.set("hgStatus", data.hg_status);
-  //       that.set("hgKey", data.hg_key);
-  //       that.set("hgSaving", false);
-  //     });
-  //   }
-  // },
-  //
-  // _baseUrl: function() {
-  //   var adapter = this.container.lookup('adapter:application');
-  //   return adapter.buildURL("");
-  // }
+  isHgFormValid: function() {
+    return this.get("isHgDisabled") || Ember.isPresent(this.get("hgFormKey"));
+  }.property("isHgDisabled", "hgFormKey"),
+
+  statusText: function() {
+    if (this.get("saving")) {
+      return "saving...";
+    } else {
+      return this.get("hgStatus");
+    }
+  }.property("hgStatus", "saving"),
+
+  actions: {
+    change: function() {
+      this.set("show", !this.get("show"));
+    },
+
+    selectEnabled: function() {
+      this.set("isHgDisabled", false);
+    },
+
+    selectDisabled: function() {
+      this.set("isHgDisabled", true);
+    },
+
+    cancel: function() {
+      this.reset();
+    },
+
+    save: function() {
+      var that = this;
+      var hgStatus = that.get('isHgDisabled') ? "disabled" : "enabled";
+      var hgKey = hgStatus === "enabled" ? that.get("hgFormKey") : "";
+      that.set("show", false);
+      that.set("saving", true);
+      Ember.$.post(that._baseUrl() + "/hg", {hg_status: hgStatus, hg_key: hgKey}, function(data) {
+        that.set("hgStatus", data.hg_status);
+        that.set("hgKey", data.hg_key);
+        that.set("saving", false);
+      });
+    }
+  },
+
+  _baseUrl: function() {
+    var adapter = this.container.lookup('adapter:application');
+    return adapter.buildURL("");
+  }
 });
