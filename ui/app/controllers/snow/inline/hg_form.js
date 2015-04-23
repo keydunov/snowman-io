0944f225a959aa7cfd2f78e3c8dc8bfb7fb93602
Alexey Vakhov <vakhov@gmail.com>
2015-04-23 12:29:46 +0300

    WIP

--- ui/app/controllers/snow/inline/hg_form.js
+++ ui/app/controllers/snow/inline/hg_form.js
@@ -1,9 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  needs: ["snow"],
-  baseUrl: Ember.computed.alias("controllers.snow.model.base_url"),
-  reportRecipients: Ember.computed.alias("controllers.snow.model.report_recipients"),
+  reset: function() {
+    console.log("reset");
+  },
+
+  // needs: ["snow"],
+  // baseUrl: Ember.computed.alias("controllers.snow.model.base_url"),
+  // reportRecipients: Ember.computed.alias("controllers.snow.model.report_recipients"),
   // hgStatus: Ember.computed.alias("controllers.snow.model.hg_status"),
   // hgKey: Ember.computed.alias("controllers.snow.model.hg_key"),
   //
