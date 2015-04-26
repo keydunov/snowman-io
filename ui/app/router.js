529fb2fa7a2d4d8fc5054a613779990eee6331b6
Alexey Vakhov <vakhov@gmail.com>
2015-04-26 14:09:56 +0300

    Be simpler

--- ui/app/router.js
+++ ui/app/router.js
@@ -23,7 +23,6 @@ Router.map(function() {
           this.route('show', {path: ':metric_id'});
           this.route('edit', {path: ':metric_id/edit'});
         });
-        this.resource('checks');
       });
     });
   });
