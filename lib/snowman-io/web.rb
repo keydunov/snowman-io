cac364ed9c6ad1ed7744245cfb1e4a0835169f0d
Artyom Keydunov <artyom.keydunov@gmail.com>
2015-04-28 13:03:52 +0300

    Refactor agent report API

--- lib/snowman-io/web.rb
+++ lib/snowman-io/web.rb
@@ -15,18 +15,6 @@ module SnowmanIO
       "PONG"
     end
 
-    post '/agent/metrics' do
-      payload = JSON.load(request.body.read)
-      if app = App.where(token: payload["token"]).first
-        payload["metrics"].each do |metric|
-          app.register_metric_value(metric["name"], metric["kind"], metric["value"].to_f, Time.now)
-        end
-        "OK"
-      else
-        "WRONG APP"
-      end
-    end
-
     # Ember application
     get '/*' do
       send_file File.expand_path("../ui/index.html", __FILE__)
